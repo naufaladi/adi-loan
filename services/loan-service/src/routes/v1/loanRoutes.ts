e63f359e5f0f0c5ab71f19842fc1600457c849c6
naufaladi <naufal.maheswara@adira.co.id>
2024-12-23 05:42:51 +0700

    add validator middleware

--- services/loan-service/src/routes/v1/loanRoutes.ts
+++ services/loan-service/src/routes/v1/loanRoutes.ts
@@ -4,6 +4,8 @@ import { LoanController } from "../../controllers/v1/loanController";
 const router = Router();
 const loan = new LoanController();
 
+// TODO request validator
+
 router.post("/", loan.createLoan);
 
 router.put("/:id/approve", loan.approveLoan);
